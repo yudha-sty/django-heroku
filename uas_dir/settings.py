25c3d477f16dfe6d8c8ed7cb3e0dda1d64057f10
unknown <setyawanyuda903@gmail.com>
2022-06-12 12:48:58 +0700

    first commit

--- uas_dir/settings.py
+++ uas_dir/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gk^y+p!$uu9^k5k0--dbvf5ytw7l!4*oica@19xbww6r!m+l6f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','uas-django.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,7 +131,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#activate django-heroku
+django_heroku.settings(locals())
--- uas_dir/urls.py
+++ uas_dir/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf import url
+from django.conf import settings
 
 from . import views
 
@@ -25,6 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('nasdaq/', include('NASDAQ.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
